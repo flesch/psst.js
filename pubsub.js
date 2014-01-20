18aa831396d5c2c75cfa8c788335b0c769e51f08
John Flesch <john@fles.ch>
2014-01-20 13:55:02 -0600

    module.exports

--- pubsub.js
+++ pubsub.js
@@ -1,51 +1,45 @@
-(function(name, definition) {
-  if (typeof define === "function") {
-    return define(definition);
-  }
-  if (typeof module !== "undefined") {
-    return module.exports = definition();
-  }
-  return this[name] = definition();
-})("pubsub", function() {
-  var is_subscribed, subscriptions;
-  subscriptions = {};
-  is_subscribed = function(channel) {
-    return Object.prototype.hasOwnProperty.call(subscriptions, channel);
-  };
-  return {
-    subscribe: function(channel, callback) {
-      if (callback instanceof Function) {
-        if (!is_subscribed(channel)) {
-          subscriptions[channel] = [];
-        }
-        subscriptions[channel].push(callback);
-        return {
-          channel: channel,
-          callback: callback
-        };
+ 
+var is_subscribed, subscriptions;
+subscriptions = {};
+is_subscribed = function(channel) {
+  return Object.prototype.hasOwnProperty.call(subscriptions, channel);
+};
+
+var pubsub = {
+  subscribe: function(channel, callback) {
+    if (callback instanceof Function) {
+      if (!is_subscribed(channel)) {
+        subscriptions[channel] = [];
       }
-    },
-    unsubscribe: function(subscription) {
-      var callback, i, _ref;
-      if (is_subscribed(subscription.channel)) {
-        _ref = subscriptions[subscription.channel];
-        for (i in _ref) {
-          callback = _ref[i];
-          if (callback === subscription.callback) {
-            subscriptions[subscription.channel].splice(i, 1);
-          }
-        }
-      }
-    },
-    publish: function(channel) {
-      var callback, i, _ref;
-      if (is_subscribed(channel)) {
-        _ref = subscriptions[channel];
-        for (i in _ref) {
-          callback = _ref[i];
-          callback.apply(this, Array.prototype.slice.call(arguments, 1));
+      subscriptions[channel].push(callback);
+      return {
+        channel: channel,
+        callback: callback
+      };
+    }
+  },
+  unsubscribe: function(subscription) {
+    var callback, i, _ref;
+    if (is_subscribed(subscription.channel)) {
+      _ref = subscriptions[subscription.channel];
+      for (i in _ref) {
+        callback = _ref[i];
+        if (callback === subscription.callback) {
+          subscriptions[subscription.channel].splice(i, 1);
         }
       }
     }
-  };
-});
+  },
+  publish: function(channel) {
+    var callback, i, _ref;
+    if (is_subscribed(channel)) {
+      _ref = subscriptions[channel];
+      for (i in _ref) {
+        callback = _ref[i];
+        callback.apply(this, Array.prototype.slice.call(arguments, 1));
+      }
+    }
+  }
+};
+
+module.exports = pubsub;
