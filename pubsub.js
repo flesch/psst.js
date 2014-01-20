b45d06e3e0295cd7bc10a8e312c5fc8e3bafdb8c
John Flesch <john@fles.ch>
2014-01-20 17:28:19 -0600

    Clean-up of coffee to js.

--- pubsub.js
+++ pubsub.js
@@ -1,45 +1,42 @@
- 
-var is_subscribed, subscriptions;
-subscriptions = {};
-is_subscribed = function(channel) {
-  return Object.prototype.hasOwnProperty.call(subscriptions, channel);
-};
-
 var pubsub = {
-  subscribe: function(channel, callback) {
+  
+  subscriptions: {},
+
+  is_subscribed: function(channel){
+    return Object.prototype.hasOwnProperty.call(this.subscriptions, channel);
+  },
+
+  subscribe: function(channel, callback){
     if (callback instanceof Function) {
-      if (!is_subscribed(channel)) {
-        subscriptions[channel] = [];
+      if (!this.is_subscribed(channel)) {
+        this.subscriptions[channel] = [];
       }
-      subscriptions[channel].push(callback);
+      this.subscriptions[channel].push(callback);
       return {
         channel: channel,
         callback: callback
       };
     }
   },
-  unsubscribe: function(subscription) {
-    var callback, i, _ref;
-    if (is_subscribed(subscription.channel)) {
-      _ref = subscriptions[subscription.channel];
-      for (i in _ref) {
-        callback = _ref[i];
-        if (callback === subscription.callback) {
-          subscriptions[subscription.channel].splice(i, 1);
+  
+  unsubscribe: function(subscription){
+    if (this.is_subscribed(subscription.channel)) {
+      for (var i in this.subscriptions[subscription.channel]) {
+        if (this.subscriptions[subscription.channel][i] === subscription.callback) {
+          this.subscriptions[subscription.channel].splice(i, 1);
         }
       }
     }
   },
-  publish: function(channel) {
-    var callback, i, _ref;
-    if (is_subscribed(channel)) {
-      _ref = subscriptions[channel];
-      for (i in _ref) {
-        callback = _ref[i];
-        callback.apply(this, Array.prototype.slice.call(arguments, 1));
+  
+  publish: function(channel){   
+    if (this.is_subscribed(channel)) {
+      for (var i = 0, len = this.subscriptions[channel].length; i < len; i++) {
+        this.subscriptions[channel][i].apply(this, Array.prototype.slice.call(arguments, 1));
       }
     }
   }
+ 
 };
 
 module.exports = pubsub;
