13a939e5dfad576d4997a1dfdd41853d8e8c8402
John Flesch <john@fles.ch>
2014-01-20 20:46:04 -0600

    Fixed off and remove redundant subscription check.

--- pubsub.js
+++ pubsub.js
@@ -27,21 +27,17 @@ var pubsub = {
     return once;
   },
   
-  off: function(subscription){    
-    if (this.is_subscribed(subscription.channel)) {
-      for (var i in this.subscriptions[subscription.channel]) {
-        if (this.subscriptions[subscription.channel][i] === subscription.callback) {
-          this.subscriptions[subscription.channel].splice(i, 1);
-        }
+  off: function(subscription){
+    for (var i = 0, len = this.subscriptions[subscription.channel].length; i < len; i++) {
+      if (this.subscriptions[subscription.channel][i] === subscription.callback) {
+        this.subscriptions[subscription.channel].splice(i, 1);
       }
     }
   },
   
   emit: function(channel){   
-    if (this.is_subscribed(channel)) {
-      for (var i = 0, len = this.subscriptions[channel].length; i < len; i++) {
-        this.subscriptions[channel][i].apply(this, Array.prototype.slice.call(arguments, 1));
-      }
+    for (var i = 0, len = this.subscriptions[channel].length; i < len; i++) {
+      this.subscriptions[channel][i].apply(this, Array.prototype.slice.call(arguments, 1));
     }
   }
   
