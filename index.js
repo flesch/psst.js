bef41782d842c9881435c4f0204700b8cc705c4a
John Flesch <john@fles.ch>
2014-01-21 10:34:13 -0600

    Error checking.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var subscriptions = {}, pubsub = {
   
   on: function(channel, callback){
-    if (Object.prototype.toString.call(callback) === '[object Function]') {
+    if (channel && Object.prototype.toString.call(callback) === '[object Function]') {
       if (!Object.prototype.hasOwnProperty.call(subscriptions, channel)) {
         subscriptions[channel] = [];
       }
@@ -22,7 +22,7 @@ var subscriptions = {}, pubsub = {
   },
   
   off: function(subscription){
-    if (Object.prototype.hasOwnProperty.call(subscription, 'channel')) {
+    if (subscription && Object.prototype.hasOwnProperty.call(subscription, 'channel')) {
       for (var i = 0, len = subscriptions[subscription.channel].length; i < len; i++) {
         if (subscriptions[subscription.channel][i] === subscription.callback) {
           subscriptions[subscription.channel].splice(i, 1);
@@ -32,7 +32,7 @@ var subscriptions = {}, pubsub = {
   },
   
   emit: function(channel){
-    if (Object.prototype.hasOwnProperty.call(subscriptions, channel)) {
+    if (channel && Object.prototype.hasOwnProperty.call(subscriptions, channel)) {
       for (var i = 0, len = subscriptions[channel].length; i < len; i++) {
         subscriptions[channel][i].apply(this, Array.prototype.slice.call(arguments, 1));
       }
